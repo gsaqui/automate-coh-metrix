473f1845582cdd1a0488680eebc57f332ec6b017
gsaqui <glenn.saqui@gmail.com>
2015-10-08 21:41:05 -0700

    First commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+You can easily install both using pip install lxml and pip install requests
---
+++ scrape.py
@@ -0,0 +1,82 @@
+from lxml import html
+import requests
+
+
+def get_data():
+    s = requests.Session()
+    starter = s.get('http://141.225.42.101/cohmetrix3/login.aspx')
+    tree = html.fromstring(starter.text)
+
+    cookies = {
+        'ASP.NET_SessionId': starter.cookies['ASP.NET_SessionId']
+    }
+
+    headers = {
+        'Host': '141.225.42.101',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:41.0) Gecko/20100101 Firefox/41.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'http://141.225.42.101/cohmetrix3/login.aspx',
+        'Connection': 'keep-alive',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+    }
+    viewstate = tree.xpath('//input[@id="__VIEWSTATE"]/@value')[0]
+    validation = tree.xpath('//input[@id="__VIEWSTATEGENERATOR"]/@value')[0]
+    view = tree.xpath('//input[@id="__EVENTVALIDATION"]/@value')[0]
+
+    data = {
+        '__VIEWSTATE': viewstate,
+        '__VIEWSTATEGENERATOR': validation,
+        '__EVENTVALIDATION': view,
+        'TextBox1': 'glenn.saqui@gmail.com',
+        'TextBox2': "bebZjcis",
+        'Button1': 'Login',
+    }
+    l = s.post('http://141.225.42.101/cohmetrix3/login.aspx', headers=headers, data=data, cookies=cookies)
+
+    headers = {
+        'Host': '141.225.42.101',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:41.0) Gecko/20100101 Firefox/41.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'http://141.225.42.101/cohmetrix3/input.aspx',
+        'Connection': 'keep-alive',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+    }
+    viewstate = "/wEPDwUJNzU4MDYzOTUyZGQKNmk5nIq1XYBgXUM1EZ1Hs0bJWQ=="
+    validation = '/wEWCgLrrJCoDQK506zcAwLy07LaDALjz837AwLBx6n3AwLptdyVCAL0geaDCwLCoveuCALpouS8DQLCi9reA0rdDT2rbbn4VrlhjGW0OKnV7hBW'
+
+    tempData = {
+        '__VIEWSTATE': viewstate,
+        '__VIEWSTATEGENERATOR': 'AD710423',
+        '__EVENTVALIDATION': validation,
+        'tbTitle': 'test',
+        'ddlGenre': 'Science',
+        'tbSource': 'source',
+        'tbUserCode': 'job+code',
+        'ddlLSASpace': 'CollegeLevel',
+        'tbInput': 'this+is+some+really+nice+code',
+        'btnSubmit': 'Submit',
+    }
+
+    t = s.post('http://141.225.42.101/cohmetrix3/input.aspx', headers=headers, cookies=cookies, data=tempData)
+    return t.text
+
+
+def parse_data(text):
+    tree = html.fromstring(text)
+    rows = tree.xpath('//tr')
+
+    for row in rows :
+        cols = row.xpath('//td')
+        if len(cols) > 3 :
+            for col in cols :
+                print col.text
+
+
+
+# resultsFromSearch = get_data()
+file = open('temp2.html', 'r')
+parse_data(file.read())
